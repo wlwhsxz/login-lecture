f3310bc4338498128c18b5aed1da3304ad8bb42d
wlwhsxz <wlwhsxz@gmail.com>
2022-06-07 12:28:33 +0900

    DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector('#id'), // html 파일에서 id에 저장된 값을 가져오긴 
+pw = document.querySelector('#pw'),
+loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener("click", login); // click 이벤트 발생 시 login 동작
+
+function login() {
+    const req = {
+        id: id.value, // xx.value 로 값 받아오기
+        pw: pw.value
+    };
+}
