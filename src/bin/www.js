6d65b0897c1557a52baeee0ed57fc2124be146f2
wlwhsxz <wlwhsxz@gmail.com>
2022-06-08 14:33:42 +0900

    src 중복 제거

--- src/bin/www.js
+++
@@ -1,8 +0,0 @@
-"use strict"; // ECMAScript(ES) 문법을 준수
-
-const app = require('../app'); // 상위 폴더로 이동, app.js을 불러오기 
-const PORT = 8080; // localhost 포트 넘버
-
-app.listen(PORT, () => {
-    console.log('서버 실행');
-});
